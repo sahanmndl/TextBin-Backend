68cd46c1a059c9936851b10f2e840c9b190941bd
sahanmndl <mondalsahan053@gmail.com>
2025-03-24 02:42:37 +0530

    trust proxy

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ dotenv.config()
 
 const app = express();
 app.set('trust proxy', 1)
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(cors());
 app.use(helmet());
 app.use(rateLimiter);
 app.use(express.json({limit: "30mb", extended: true}));
