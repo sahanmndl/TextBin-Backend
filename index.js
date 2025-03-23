0b1fac1dc8a4d94d06c037db444f2094faa2a273
sahanmndl <mondalsahan053@gmail.com>
2025-03-24 02:42:10 +0530

    trust proxy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import documentRoutes from "./routes/DocumentRoute.js";
 dotenv.config()
 
 const app = express();
-app.set('trust proxy', true)
+app.set('trust proxy', 1)
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
