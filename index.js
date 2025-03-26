fd161bd2c6e40c32d759d78363142baa27340cfa
sahanmndl <mondalsahan053@gmail.com>
2025-03-27 01:40:47 +0530

    changes

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import {rateLimiter} from "../config/rateLimiter.js";
-import documentRoutes from "../routes/DocumentRoute.js";
-import {connectToDatabase} from "../config/database.js";
-import {connectToCacheDB} from "../config/cache.js";
-import {logger} from "../config/logger.js";
+import {rateLimiter} from "./config/rateLimiter.js";
+import documentRoutes from "./routes/DocumentRoute.js";
+import {logger} from "./config/logger.js";
+import {connectToDatabase} from "./config/database.js";
+import {connectToCacheDB} from "./config/cache.js";
 
 dotenv.config()
 
 const app = express();
-app.set('trust proxy', 1)
+// app.set('trust proxy', 1)
 app.use(cors());
 app.use(helmet());
 app.use(rateLimiter);
 app.use(express.json({limit: "30mb", extended: true}));
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use("/api/v1/documents", documentRoutes);
 
 app.listen((process.env.PORT || 8008), async () => {
